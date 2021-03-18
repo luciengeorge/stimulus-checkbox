03b220d256c4434590b8aa527d2575ce61c9dd21
Lucien George <luciengeorge95@gmail.com>
2021-03-18 16:29:38 +0000

    added more tests and updated state of input on connect

--- tests/checkbox_controller_test.js
+++ tests/checkbox_controller_test.js
@@ -1,24 +1,25 @@
 import { Application } from "stimulus";
 import CheckboxController from "index";
 
+let input, boxes;
+
 describe("CheckboxController", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+    <div data-controller="checkbox">
+      <input type="checkbox" id="select-all" data-checkbox-target="btn" data-action="change->checkbox#toggle">
+      <input type="checkbox" data-checkbox-target="box" data-action="change->checkbox#toggleOne">
+      <input type="checkbox" data-checkbox-target="box" data-action="change->checkbox#toggleOne">
+      <input type="checkbox" data-checkbox-target="box" data-action="change->checkbox#toggleOne">
+    </div>`;
+
+    const application = Application.start();
+    application.register("checkbox", CheckboxController);
+    input = document.getElementById("select-all");
+    boxes = document.querySelectorAll('[data-checkbox-target="box"]');
+  });
   describe("#toggle", () => {
-    beforeEach(() => {
-      document.body.innerHTML = `
-      <div data-controller="checkbox">
-        <input type="checkbox" id="select-all" data-checkbox-target="btn" data-action="change->checkbox#toggle">
-        <input type="checkbox" data-checkbox-target="box">
-        <input type="checkbox" data-checkbox-target="box">
-        <input type="checkbox" data-checkbox-target="box">
-      </div>`;
-
-      const application = Application.start();
-      application.register("checkbox", CheckboxController);
-    });
-
     it("checks all the checkboxes", () => {
-      const input = document.getElementById("select-all");
-      const boxes = document.querySelectorAll('[data-checkbox-target="box"]');
       input.click();
 
       expect(Array.from(boxes).filter(box => box.checked).length).
@@ -26,14 +27,28 @@ describe("CheckboxController", () => {
     });
 
     it("unchecks all the checkboxes if they are already checked", () => {
-      const input = document.getElementById("select-all");
-      const boxes = document.querySelectorAll('[data-checkbox-target="box"]');
       input.click();
       input.click();
 
       // This line has changed its expectations.
       expect(Array.from(boxes).filter(box => box.checked).length).
         toEqual(0);
-    })
+    });
   });
+
+  describe("#toggleOne", () => {
+    it("checks that the input is checked if all checkboxes are checked", () => {
+      boxes.forEach(box => box.click());
+      expect(input.checked).toEqual(true);
+    });
+
+    it("checks that the input is not checked if no checkboxes are checked", () => {
+      expect(input.checked).toEqual(false);
+    });
+
+    it("checks that the input is in an indeterminate state if some checkboxes are checked", () => {
+      boxes[0].click();
+      expect(input.indeterminate).toEqual(true);
+    });
+  })
 });
